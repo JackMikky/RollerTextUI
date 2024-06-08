401f9558dd547ba17f351e10c96c72d2088d9c52
JackMikky <76462734+JackMikky@users.noreply.github.com>
2024-06-08 14:08:40 +0900

    [Roller Text] Project initialization done

---
+++ Assets/TextEffect.cs
@@ -0,0 +1,93 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.EventSystems;
+
+public class TextEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+
+{
+    enum TextSituation
+    {
+        AnimationActived = 0,
+        AnimationInactivated =1,
+        AnimateAlways=-1
+    }
+    enum MoveDirection
+    {
+        Right = 1,
+        Left = 0
+    }
+    TMP_Text _Text;
+    string textCash;
+    string oringeText;
+    [SerializeField] TextSituation situation = TextSituation.AnimationInactivated;
+    [SerializeField] MoveDirection direction = MoveDirection.Left;
+    [Tooltip("When pointer exit UI reset text position")]
+    [SerializeField] bool resetText =true;
+    [Tooltip("Text move speed (perChar/second)")]
+    [Range(0.01f, 1)]
+    [SerializeField] float moveTimer = 0.25f;
+    float timer;
+
+    void Awake()
+    {
+        _Text = GetComponent<TMP_Text>();
+        oringeText = _Text.text;
+        textCash = _Text.text + "            ";
+    }
+    void FixedUpdate()
+    {
+        if (situation is 0 or (TextSituation)(-1))
+        {
+            timer += Time.fixedDeltaTime;
+            MoveText();
+        }
+    }
+    void MoveText()
+    {
+        if (timer >= moveTimer)
+        {
+            char textTemp;
+            string newText;
+            switch (direction)
+            {
+                case MoveDirection.Right:
+                    newText = textCash[..(textCash.Length-1)];
+                    textTemp = textCash[^1];
+                    textCash = textTemp+ newText;
+                    _Text.text = textCash;
+                    break;
+                case MoveDirection.Left:
+                    newText = textCash[1..];
+                    textTemp = textCash[0];
+                    textCash = newText + textTemp;
+                    _Text.text = textCash;
+                    break;
+            }
+            timer = 0;
+        }
+    }
+
+    /// <summary>
+    /// index 0> AnimationActived 1> AnimationInactivated
+    /// </summary>
+    /// <param name="situationIndex"></param>
+    void SwitchSituation(byte situationIndex)
+    {
+        situation = (TextSituation)situationIndex;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        SwitchSituation(0);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        SwitchSituation(1);
+        if (resetText)
+        {
+            textCash = oringeText + "            ";
+        }
+        _Text.text = oringeText;
+    }
+}
